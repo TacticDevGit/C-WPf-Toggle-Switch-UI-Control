8dc08c64ce19327c403c4eb264ed40c830e2a310
Tactic Apps <38791845+TacticApps@users.noreply.github.com>
2018-05-12 23:43:32 +0800

    Add files via upload

---
+++ ToggleSwitch/ToggleSwitch/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToggleSwitch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SolidColorBrush Off = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        SolidColorBrush On = new SolidColorBrush(Color.FromRgb(240, 222, 45));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Bu_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Bu.Toggled1 == true)
+            {
+                Light.Fill = On;
+
+
+            }
+            else
+            {
+
+                Light.Fill = Off;
+            }
+
+
+        }
+    }
+}
